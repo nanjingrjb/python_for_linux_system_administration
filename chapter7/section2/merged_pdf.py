b894b2610d3be272973d81100f83b3d382da7fe5
nanjingrjb <nanjingrjb@gmail.com>
2023-07-16 18:07:38 +0800

    增加合并所有pdf文件的函数mergeAllpdf() 以及增加将某个pdf文件插入到本目录下的其他pdf文件后面。 主要步骤为： 1）创建一个合并数据结构 mg=PyPDF2.PdfMerge() 2)读取第一个文件，并合并     with open('first.pdf','rb') as fst:        mg.append(fst) 3)读取要插入的文件         with open(insertpdfname, 'rb') as sec_obj:             reader = PyPDF2.PdfReader(sec_obj)             merger.append(fileobj=sec_obj, pages=(0, len(reader.pages))) 4）写入文件          newpdf =pdf[0:-5]+"_add.pdf"             with open(newpdf, 'wb') as f:                 merger.write(f)

--- chapter7/section2/merged_pdf.py
+++ chapter7/section2/merged_pdf.py
@@ -9,23 +9,54 @@ def get_all_pdf_files(path):
     return all_pdfs
 
 
-def main():
-    all_pdfs = get_all_pdf_files(os.path.expanduser('~lmx/'))
+#将所有pdf文件合并成一个文件
+def mergeAllpdf():
+    all_pdfs = get_all_pdf_files(os.path.expanduser('.'))
     if not all_pdfs:
         raise SystemExit('No pdf file found!')
 
-    merger = PyPDF2.PdfFileMerger()
+    merger = PyPDF2.PdfMerger()
 
     with open(all_pdfs[0], 'rb') as first_obj:
         merger.append(first_obj)
 
     for pdf in all_pdfs[1:]:
         with open(pdf, 'rb') as obj:
-            reader = PyPDF2.PdfFileReader(obj)
-            merger.append(fileobj=obj, pages=(1, reader.getNumPages()))
+            reader = PyPDF2.PdfReader(obj)
+            merger.append(fileobj=obj, pages=(0, len(reader.pages)))
 
     with open('merge-pdfs.pdf', 'wb') as f:
         merger.write(f)
 
+#将列表中的某个pdf插入其他pdf后面
+def insertOne2End(insertpdfname=".\\end.pdf"):
+    all_pdfs = get_all_pdf_files(os.path.expanduser('.'))
+    if not all_pdfs:
+        raise SystemExit('No pdf file found!')
+
+
+    #排除指定的pdf文件
+    if insertpdfname in all_pdfs:
+        all_pdfs.remove(insertpdfname)
+
+
+    for pdf in all_pdfs[0:]:
+        merger = PyPDF2.PdfMerger()
+
+        with open(pdf, 'rb') as first_obj:
+            merger.append(first_obj)
+        with open(insertpdfname, 'rb') as sec_obj:
+            reader = PyPDF2.PdfReader(sec_obj)
+            merger.append(fileobj=sec_obj, pages=(0, len(reader.pages)))
+            newpdf =pdf[0:-5]+"_add.pdf"
+            with open(newpdf, 'wb') as f:
+                merger.write(f)
+
+
+
+
+
+
 if __name__ == '__main__':
-    main()
+    #mergeAllpdf()
+    insertOne2End()
