4004dd7e672910878299dae49ea647701c4f1c60
nanjingrjb <nanjing@gmail.com>
2023-07-19 10:36:37 +0800

    新增函数，两种登录南京图书馆的方法，其中手机验证码目前还未完全弄好。
    以后寻找网页元素推荐采用find_element(By.XPATH,'//input[@attr1="val"]')
    这种寻找元素非常有效。

--- login_njlib.py
+++ login_njlib.py
@@ -4,52 +4,75 @@ from selenium.webdriver.common.by import By
 #模拟鼠标操作
 from selenium.webdriver.common.action_chains import ActionChains
 
-driver = webdriver.Edge()
+def login_sj():
+    driver = webdriver.Edge()
 
-driver.get('http://ntszzy.org:8070/ermsLogin/view.do')
-time.sleep(2)
-# 定位搜索输入框
-#采用输入加属性的办法来实现，其中
-text_label =driver.find_element(By.XPATH,'//input[@name="userName"]')
-text_label.send_keys('13776603610')
-#ac=ActionChains(driver)
-#ac.click(text_label).perform()
-text_label =driver.find_element(By.XPATH,'//input[@name="password"]')
-text_label.send_keys('Scusky111111')
-text_label =driver.find_element(By.XPATH,'//input[@name="cwcode"]')
-text_label.send_keys('3')
+    driver.get('http://ntszzy.org:8070/ermsLogin/view.do')
+    time.sleep(2)
+    # 定位搜索输入框
+    #采用输入加属性的办法来实现，其中
+    text_label =driver.find_element(By.XPATH,'//input[@name="userName"]')
+    text_label.send_keys('13776603610')
+    #ac=ActionChains(driver)
+    #ac.click(text_label).perform()
+    text_label =driver.find_element(By.XPATH,'//input[@name="password"]')
+    text_label.send_keys('Scusky111111')
+    text_label =driver.find_element(By.XPATH,'//input[@name="cwcode"]')
+    val=input("input digit res")
+    text_label.send_keys(str(val))
 
-#捕捉登录按钮
-login_butt =driver.find_element(By.XPATH,'//input[@type="button"]')
-ac=ActionChains(driver)
-#点击登录按钮
-ac.click(login_butt).perform()
-time.sleep(10)
+    #捕捉登录按钮
+    login_butt =driver.find_element(By.XPATH,'//input[@type="button"]')
+    ac=ActionChains(driver)
+    #点击登录按钮
+    ac.click(login_butt).perform()
+    time.sleep(100)
+
+#手机验证码登录
+def login_yzm():
+    driver = webdriver.Edge()
+
+    driver.get('http://ntszzy.org:8070/ermsLogin/view.do')
+    time.sleep(2)
+    # 定位搜索输入框
+    #采用输入加属性的办法来实现，其中
+    text_label =driver.find_element(By.XPATH,'//a[@href="#tabs-2"]')
+    #选择点击
+    ac=ActionChains(driver)
+    #点击登录按钮
+    ac.click(text_label).perform()
+
+     #采用输入加属性的办法来实现，其中
+    text_label =driver.find_element(By.XPATH,'//input[@name="phone"]')
+    text_label.send_keys('13776603610')
+
+    #获取验证码
+    #text_label =driver.find_element(By.XPATH,'//a[@href="javascript:void(0);"]')
+    text_label=driver.find_element(By.CLASS_NAME,'send-btn-link')
+    #选择点击
+    ac.click(text_label).perform()
+    time.sleep(2)
+    text_label =driver.find_element(By.XPATH,'//input[@name="smscode"]')
+    code=input("输入校验码")
+ 
+    text_label.send_keys(str(code))
+    time.sleep(2)
+
+    #点击登录按钮
+    ac.click(text_label).perform()
+     #捕捉登录按钮
+    login_butt =driver.find_element(By.XPATH,'//input[@type="button"]')
+    ac=ActionChains(driver)
+    #点击登录按钮
+    ac.click(login_butt).perform()
+
+    time.sleep(100)
+
+if __name__=='__main__':
+    login_sj()
+    #login_yzm()
 
 
-# 在搜索框中输入 Dream丶Killer
 
 
 
-time.sleep(200)
-# 清除搜索框中的内容
-text_label.clear()
-
-# 输出搜索框元素是否可见
-print(text_label.is_displayed())
-# 输出placeholder的值
-print(text_label.get_attribute('placeholder'))
-
-# 定位搜索按钮
-button = driver.find_element_by_xpath('//*[@id="toolbar-search-button"]/span')
-# 输出按钮的大小
-print(button.size)
-# 输出按钮上的文本
-print(button.text)
-
-'''输出内容
-True
-python面试100问
-{'height': 32, 'width': 28}
-搜索
-'''
